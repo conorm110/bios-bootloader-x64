7c07c707876a044bf64111a0eb1ffd3a6fe13b69
conorm110 <cjmika110@gmail.com>
2021-12-23 17:36:09 -0600

    simplified boot16

--- boot16.asm
+++ boot16.asm
@@ -1,54 +1,13 @@
 [org 0x7c00]
 
-mov [BOOT_DISK], dl
+mov ah, 0x02          ; read sectors from drive
+mov bx, 0x7e00        ; buffer address pointer
+mov al, 128           ; number of sectors to read
+mov ch, 0x00          ; cylinder number
+mov dh, 0x00          ; head number
+mov cl, 0x02          ; sector number
+int 0x13              ; read disk to memory
+jmp 0x7e00            ; jump to the kernel
 
-mov bp, 0x7c00
-mov sp, bp
-
-mov ah, 0x02
-mov bx, PROGRAM_SPACE
-mov al, 4
-mov dl, [BOOT_DISK]
-mov ch, 0x00
-mov dh, 0x00
-mov cl, 0x02
-
-int 0x13
-
-jc disreaderr
-
-jmp PROGRAM_SPACE
-
-PROGRAM_SPACE equ 0x7e00
-
-
-BOOT_DISK:
-    db 0
-
-diskreaderrstr:
-    db 'boot16 could not load extended program space',0
-
-disreaderr:
-    mov bx, diskreaderrstr ; move the string to be printed into bx
-    push ax                ; store ax in stack
-    push bx                ; store bx in stack
-
-    ; print the string by looping through and printing characters
-    mov ah, 0x0e
-    .Loop:
-    cmp [bx], byte 0
-    je .Exit
-        mov al, [bx]
-        int 0x10
-        inc bx
-        jmp .Loop
-    .Exit:
-
-    pop ax                 ; restore ax from stack
-    pop bx                 ; restore bx from stack
-
-    jmp $
-    
 times 510-($-$$) db 0
-
 dw 0xaa55
